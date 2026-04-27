b0927cca87564d1e8f0457db81672c52fe40363a
CarrJ <james.carr@dsit.gov.uk>
2026-04-27 21:01:39 +0100

    - rename test files - move into a single feature file - simplify test step definitions - add new markdown test files

---
+++ features/rules/style_guide/acronyms.feature
@@ -0,0 +1,34 @@
+Feature: Acronyms are defined in technical documentation
+    No-one likes likes to not know what an acronym means
+
+    Scenario: A page has been created with a single unknown acronym that has not been defined in full
+        Given a page contains an acronym
+        And the acronym "is not" considered well known
+        And it "has not" been defined in the first usage
+        When the linter runs against the page with the "acronyms" rule
+        Then the linter should "fail"
+        And the number of errors in the linter report should be 1
+        And the error should include "must be defined in the first instance"
+
+    Scenario: A page has been created with a single unknown acronym that has been defined in full
+        Given a page contains an acronym
+        And the acronym "is not" considered well known
+        And it "has" been defined in the first usage
+        When the linter runs against the page with the "acronyms" rule
+        Then the linter should "pass"
+
+    Scenario: A page has been created with a widley known acronym that has been defined in full
+        Given a page contains an acronym
+        And the acronym "is" considered well known
+        And it "has" been defined in the first usage
+        When the linter runs against the page with the "acronyms" rule
+        Then the linter should "fail"
+        And the number of errors in the linter report should be 1
+        And the error should include "something"
+
+    Scenario: A page has been created with a widley known acronym, and not defined unnecessarily
+        Given a page contains an acronym
+        And the acronym "is" considered well known
+        And it "has not" been defined in the first usage
+        When the linter runs against the page with the "acronyms" rule
+        Then the linter should "pass"
--- features/step_definitions/acronyms.rb
+++ features/step_definitions/acronyms.rb
@@ -1,6 +1,5 @@
 require_relative "../support/run_vale_rule"
 
-
 module ValeWorld 
   def vale_result
     @vale_result
@@ -17,9 +16,9 @@ Given('a page contains an acronym') do
   @dir = "acronyms"
 end
 
-Given('the acronym {string} on the exceptions list') do |is_not|
+Given('the acronym {string} considered well known') do |is_not|
   if is_not == "is not"
-    @sub = "no-exceptions"
+    @sub = "not-on-exceptions-list"
   elsif is_not == "is"
     @sub = "exceptions"
   end
@@ -27,7 +26,7 @@ end
 
 Given('it {string} been defined in the first usage') do |has_not|
   if has_not == "has"
-    @page = "defined"
+    @page = "is-defined"
   elsif has_not == "has not"
     @page = "not-defined"
   end
@@ -35,7 +34,7 @@ end
 
 When('the linter runs against the page with the {string} rule') do |rule_name|
   self.vale_result = ValeRunner.run(
-    file_path: "build/#{@dir}/#{@sub}/",
+    file_path: "build/#{@dir}/#{@sub}/#{@page}/",
     filter: rule_name
   )
   expect(vale_result.status).not_to be_nil
@@ -56,22 +55,28 @@ end
 #  }
 # ]
 #}
-Then('the linter should fail with {int} error') do |number_of_failures|
+Then('the linter should {string}') do |pass_or_fail|
   # we have specified a single page so we expect the top object length to be 1.  This is just a defensive test before we go to the actual errors
-  expect(vale_result.json.keys.count).to eq 1 
+  if "pass" == pass_or_fail
+    expect(vale_result.status.exitstatus).to eq(0)
+  elsif "fail" == pass_or_fail
+    expect(vale_result.status.exitstatus).to eq(1)
+  else
+    raise NotImplementedError, "Unimplemented linter: #{pass_or_fail}"
+  end
 
+end
+
+Then('the number of errors in the linter report should be {float}') do |number_of_errors|
+  # we have specified a single page so we expect the top object length to be 1.  This is just a defensive test before we go to the actual errors
   vale_result_key = vale_result.json.keys[0]
   @vale_result_json = vale_result.json[vale_result_key]
-  expect(@vale_result_json.count).to eq number_of_failures 
+  expect(@vale_result_json.count).to eq number_of_errors
 end
 
 Then('the error should include {string}') do |error_message|
   expect(@vale_result_json[0]).to have_key("Message")
   expect(@vale_result_json[0]["Message"]).to include(error_message)
-end
-
-Then('the linter should pass') do
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Given('the {string} list') do |string|
