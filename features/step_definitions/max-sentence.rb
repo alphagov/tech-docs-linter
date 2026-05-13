8ed600db3f978eb5275dca5e3214820aef6ded81
CarrJ <james.carr@dsit.gov.uk>
2026-05-13 09:10:12 +0100

    - pending steps to be implemented for max sentence

---
+++ features/step_definitions/max-sentence.rb
@@ -0,0 +1,27 @@
+Given('the page has {string}') do |number_of_sentences|
+  pending
+end
+
+Given('the number of long sentences is {int}') do |number_of_sentences|
+  pending
+end
+
+Given('the number of short sentences is {int}') do |number_of_sentences|
+  pending
+  end
+
+Given('the number of items in the list is {int}') do |number_of_sentences|
+  pending
+end
+
+Given('the number of long lead in lines is {int}') do |number_of_sentences|
+  pending
+end
+
+Given('the number of long list items is {int}') do |number_of_sentences|
+  pending
+end
+
+Given('the total length of the sentence is {string} words') do |number_of_sentences|
+  pending
+end
